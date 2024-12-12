1e50a97626cb5f36d4db1adb91aaa43107dde352
RutujaWayase <rutujawayase1234@gmail.com>
2024-12-12 23:59:27 +0530

    adding filter

---
+++ seleniumWebDriver/src/test/java/SeleniumInterviewQuestions/Filter.java
@@ -0,0 +1,24 @@
+package SeleniumInterviewQuestions;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Filter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//textarea[@name='q']")).sendKeys("wheat");
+		
+		//Collect list of items
+		List<WebElement> vegs = driver.findElements(By.xpath(""));
+
+	}
+
+}
