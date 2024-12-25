d7fb1266bf25eb1fcf559dc063498006dfb247eb
RutujaWayase <rutujawayase1234@gmail.com>
2024-12-25 22:21:17 +0530

    adding first non repeated char in string provided scenario

--- seleniumWebDriver/src/test/java/CodingQuestions/FirstNonRepeatingCharacterString.java
+++ seleniumWebDriver/src/test/java/CodingQuestions/FirstNonRepeatingCharacterString.java
@@ -1,5 +1,34 @@
 package CodingQuestions;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class FirstNonRepeatingCharacterString {
+    public static void main(String[] args){
+        //String str = "swiss";
+        String str = "Rutuja Wayase";
+        Map<Character, Integer> map = new LinkedHashMap<Character, Integer>();
+        int count = 1;
+
+        for(int i=0; i<str.length(); i++){
+            if(!map.containsKey(str.charAt(i))){
+                map.put(str.charAt(i), count);
+            }else{
+                map.put(str.charAt(i), map.get(str.charAt(i))+1);
+            }
+        }
+
+        //printing the first non-repeated character from map
+        
+        for(Entry<Character, Integer> entry : map.entrySet()){
+            if(entry.getValue()==1){
+                System.out.println("The first non repeated character is: "+ entry.getKey());
+                break;
+            }
+        }
+
+    }
     
 }
+ 
