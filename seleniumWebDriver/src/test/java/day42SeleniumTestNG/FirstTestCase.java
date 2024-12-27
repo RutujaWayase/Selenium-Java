952c2905c7699a86d8e190c15505b22aff45cb28
RutujaWayase <rutujawayase1234@gmail.com>
2024-12-27 23:59:25 +0530

    adding testng

---
+++ seleniumWebDriver/src/test/java/day42SeleniumTestNG/FirstTestCase.java
@@ -0,0 +1,28 @@
+package day42SeleniumTestNG;
+
+
+/*
+1) Open app
+2) Login
+3) Logout
+*/
+
+public class FirstTestCase {
+	
+	void openapp() {
+		System.out.println("Opening application....");
+		
+	}
+	
+	void login() {
+		System.out.println("Login to application....");
+		
+	}
+	
+	void logout() {
+		System.out.println("Logout from application....");
+		
+	}
+	
+
+}
